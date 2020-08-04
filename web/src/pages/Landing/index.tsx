23380563f040282fc6f2328c978fdfcc02f402ee
Sérgio Carvalho <sergiocarvalho.m@outlook.com>
2020-08-03 22:18:55 -0300

    Change a tag to Link component

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './styles.css';
 
 // Assets
@@ -20,15 +21,15 @@ function Landing() {
         <img src={landingImg} alt="" className="hero-image" />
         
         <div className="buttons-container">
-          <a href="" className="study">
+          <Link to="/study" className="study">
             <img src={studyIcon} alt="Estudar" />
             Estudar
-          </a>
+          </Link>
           
-          <a href="" className="give-classes">
+          <Link to="/teach" className="give-classes">
             <img src={giveClassesIcon} alt="Dar aulas" />
             Dar aulas
-          </a>
+          </Link>
         </div>
         
         <span className="total-connections">
