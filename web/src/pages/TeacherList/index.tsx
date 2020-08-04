24d21df79e7d461605ae6bce6b19404cce67bbff
Sérgio Carvalho <sergiocarvalho.m@outlook.com>
2020-08-04 15:06:47 -0300

    Style filter header on teacher list page

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -7,7 +7,24 @@ import PageHeader from '../../components/PageHeader';
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
-      <PageHeader title="Estes são os proffys disponíveis." />
+      <PageHeader title="Estes são os proffys disponíveis.">
+        <form id="search-teachers">
+          <div className="input-block">
+            <label htmlFor="subject">Matéria</label>
+            <input type="text" id="subject" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="week_day">Dia da Semana</label>
+            <input type="text" id="week_day" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="hour">Hora</label>
+            <input type="text" id="hour" />
+          </div>
+        </form>
+      </PageHeader>
     </div>
   );
 }
