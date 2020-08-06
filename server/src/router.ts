8a2327390252d84da712843966fd1bc68f8df64a
Sérgio Carvalho <sergiocarvalho.m@outlook.com>
2020-08-06 15:30:24 -0300

    Implement connection counter

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import LeassonsController from './controllers/LeassonsController';
+import ConnectionsController from './controllers/ConnectionsControllers';
 
 const router = Router();
 const leassonsController = new LeassonsController();
+const connectionsController = new ConnectionsController();
 
 router.get('/leassons', leassonsController.index);
 router.post('/leassons', leassonsController.create);
+
+router.post('/connections', connectionsController.create);
 
 export default router;
