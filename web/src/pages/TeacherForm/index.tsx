d1ac27bf7d299fb65ee974cf786983e55313f9f4
Sérgio Carvalho <sergiocarvalho.m@outlook.com>
2020-08-06 18:29:17 -0300

    Add first block of data in teach form

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import './styles.css';
+
+// Components
+import PageHeader from '../../components/PageHeader';
 
 function TeacherForm() {
-  return <h1>Teacher Form</h1>
+  return (
+    <div id="page-teacher-form" className="container">
+      <PageHeader
+        title="Que incrível que você quer ensinar."
+        description="O primeiro passo é preencher esse formulário de inscrição."
+      />
+
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+
+          <div className="input-block">
+            <label htmlFor="name">Nome completo</label>
+            <input type="text" id="name" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="avatar">Avatar</label>
+            <input type="text" id="avatar" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="whatsapp">WhatsApp</label>
+            <input type="text" id="whatsapp" />
+          </div>
+        </fieldset>
+      </main>
+    </div>
+  );
 }
 
 export default TeacherForm;
