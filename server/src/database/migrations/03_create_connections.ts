e314180e983b125eb7c1976ebf67b5226ade6e8b
Sérgio Carvalho <sergiocarvalho.m@outlook.com>
2020-08-06 15:39:54 -0300

    Fix create connection timestamp

--- server/src/database/migrations/03_create_connections.ts
+++ server/src/database/migrations/03_create_connections.ts
@@ -10,7 +10,7 @@ export async function up(knex: Knex) {
       .inTable('users');
 
     table.timestamp('created_at')
-      .defaultTo('now()')
+      .defaultTo(knex.raw('CURRENT_TIMESTAMP'))
       .notNullable();
   });
 }
