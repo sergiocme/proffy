ca570a61ceb41bee9b0c7c6467c0817650159cd6
Sérgio Carvalho <sergiocarvalho.m@outlook.com>
2020-08-07 20:58:18 -0300

    Fetch total connection for landing page

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './styles.css';
+
+// Services
+import api from '../../services/api';
 
 // Assets
 import logoImg from '../../assets/images/logo.svg';
@@ -10,6 +13,15 @@ import teachIcon from '../../assets/images/icons/teach.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
 function Landing() {
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('/connections')
+    .then(({ data }) => {
+      setTotalConnections(Number(data.total_connections));
+    });
+  }, []);
+
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
@@ -17,28 +29,27 @@ function Landing() {
           <img src={logoImg} alt="Proffy" />
           <h2>Sua plataforma de estudos online.</h2>
         </div>
-        
+
         <img src={landingImg} alt="" className="hero-image" />
-        
+
         <div className="buttons-container">
           <Link to="/study" className="study">
             <img src={studyIcon} alt="" />
             Estudar
           </Link>
-          
+
           <Link to="/teach" className="teach">
             <img src={teachIcon} alt="" />
             Ensinar
           </Link>
         </div>
-        
+
         <span className="total-connections">
-          Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="" />.
+          Total de {totalConnections} conexões já realizadas <img src={purpleHeartIcon} alt="" />.
         </span>
       </div>
     </div>
     );
   }
-  
+
   export default Landing;
-  
