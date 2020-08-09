0dc220ab0c56f7a9a863684475cc5e85bd263201
Sérgio Carvalho <sergiocarvalho.m@outlook.com>
2020-08-09 20:09:30 -0300

    Create Teach screen

--- mobile/src/pages/Teach/index.tsx
+++ mobile/src/pages/Teach/index.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useCallback } from 'react';
+import { View, ImageBackground, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
+import styles from './styles';
+
+// Assets
+import giveClassesBackgroungImage from '../../assets/images/give-classes-background.png';
 
 function Teach() {
+  const { goBack } = useNavigation();
+
+  const handleGoBack = useCallback(() => {
+    goBack();
+  }, []);
+
   return (
-    <View />
+    <View style={styles.container}>
+      <ImageBackground
+        source={giveClassesBackgroungImage}
+        resizeMode="contain"
+        style={styles.content}
+      >
+        <Text style={styles.title}>Quer ser um Proffy?</Text>
+        <Text style={styles.description}>
+          Para começar, você precisa se cadastrar como professor na nossa plataforma web.
+        </Text>
+      </ImageBackground>
+
+      <RectButton onPress={handleGoBack} style={styles.okayButton}>
+        <Text style={styles.okayButtonText}>Tudo bem</Text>
+      </RectButton>
+    </View>
   );
 }
 
