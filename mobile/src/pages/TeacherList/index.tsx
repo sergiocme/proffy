5f43819de326d6f6f63fbd0d29fff0589a143841
Sérgio Carvalho <sergiocarvalho.m@outlook.com>
2020-08-09 21:06:33 -0300

    Style Teacher List screen tab

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { View } from 'react-native';
+import styles from './styles';
 
 function TeacherList() {
   return (
-    <View />
+    <View style={styles.container} />
   );
 }
 
--- mobile/src/routers/TeacherTabRouter.tsx
+++ mobile/src/routers/TeacherTabRouter.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 // Components
@@ -10,9 +11,48 @@ const { Navigator, Screen } = createBottomTabNavigator();
 
 function TeacherTabRouter() {
   return (
-    <Navigator>
-      <Screen name="Landing" component={TeacherList} />
-      <Screen name="Teach" component={Favorites} />
+    <Navigator tabBarOptions={{
+      style: {
+        elevation: 0,
+        shadowOpacity: 0,
+        height: 64,
+      },
+      tabStyle: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      iconStyle: {
+        flex: 0,
+        width: 20,
+        height: 20,
+      },
+      labelStyle: {
+        marginLeft: 16,
+        fontFamily: 'Archivo_700Bold',
+        fontSize: 13,
+      },
+      activeBackgroundColor: '#ebebf5',
+      activeTintColor: '#32264d',
+      inactiveBackgroundColor: '#fafafc',
+      inactiveTintColor: '#c1bccc',
+    }}>
+      <Screen
+        name="Teachers"
+        component={TeacherList}
+        options={{
+          tabBarLabel: 'Proffys',
+          tabBarIcon: ({ size, color }) => <Ionicons name="ios-easel" size={size} color={color} />,
+        }}
+      />
+      <Screen
+        name="Favorites"
+        component={Favorites}
+        options={{
+          tabBarLabel: 'Favoritos',
+          tabBarIcon: ({ size, color }) => <Ionicons name="ios-heart" size={size} color={color} />,
+        }}
+      />
     </Navigator>
   );
 }
