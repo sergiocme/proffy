1108a2c49668c7d98b489d9f52321f7d2864a222
Sérgio Carvalho <sergiocarvalho.m@outlook.com>
2020-08-10 22:10:21 -0300

    Add input filters on teacher list

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, ScrollView } from 'react-native';
+import { View, ScrollView, Text, TextInput } from 'react-native';
 import styles from './styles';
 
 // Components
@@ -9,7 +9,36 @@ import TeacherItem from '../../components/TeacherItem';
 function TeacherList() {
   return (
     <View style={styles.container}>
-      <PageHeader title="Proffys disponíveis" />
+      <PageHeader title="Proffys disponíveis">
+        <View style={styles.searchForm}>
+          <Text style={styles.label}>Matéria</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Qual a matéria?"
+            placeholderTextColor="#c1bccc"
+          />
+
+          <View style={styles.inputGroup}>
+            <View style={styles.inputBlock}>
+              <Text style={styles.label}>Dia da semana</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Qual o dia?"
+                placeholderTextColor="#c1bccc"
+              />
+            </View>
+
+            <View style={styles.inputBlock}>
+              <Text style={styles.label}>Horário</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Qual horário?"
+                placeholderTextColor="#c1bccc"
+              />
+            </View>
+          </View>
+        </View>
+      </PageHeader>
 
       <ScrollView
         style={styles.teacherList}
--- mobile/src/pages/TeacherList/styles.ts
+++ mobile/src/pages/TeacherList/styles.ts
@@ -7,8 +7,36 @@ const styles = StyleSheet.create({
   },
 
   teacherList: {
-    marginTop: -60,
+    marginTop: -40,
   },
+
+  searchForm: {
+    marginBottom: 8,
+  },
+
+  label: {
+    fontFamily: 'Poppins_400Regular',
+    color: '#d4c2ff',
+  },
+
+  inputGroup: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+
+  inputBlock: {
+    width: '48%',
+  },
+
+  input: {
+    justifyContent: 'center',
+    height: 54,
+    marginTop: 4,
+    marginBottom: 16,
+    paddingHorizontal: 16,
+    borderRadius: 8,
+    backgroundColor: '#fff',
+  }
 });
 
 export default styles;
